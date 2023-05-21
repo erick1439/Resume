df0c17c8ae886aeb7ed4900901388e37293dc14d
erick1439 <erick1439@live.com>
2023-05-21 00:22:11 -0400

    style change to remove margin between company and role

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -73,7 +73,7 @@ const Resume = ({resume}) => {
 
       <div className="row work">
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Work Experience</span></h1>
          </div>
          <div className="nine columns main-col">
           {work}
